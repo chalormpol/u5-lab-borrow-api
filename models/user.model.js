17b5700835c717a007e1ed9a7e5aa9ca8a461cdd
unknown <chalormpol9899@gmail.com>
2026-01-22 11:15:46 +0700

    Second commit

---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    displayName: { type: String, required: true, trim: true },
+    passwordHash: { type: String, required: true },
+    role: { type: String, enum: ["staff", "admin"], default: "staff" },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", userSchema);
