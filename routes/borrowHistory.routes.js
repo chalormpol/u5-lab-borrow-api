3ae0ff24d4d17daa56b1291fbeabde443c818b99
unknown <chalormpol9899@gmail.com>
2026-02-16 12:07:52 +0700

    fig routes

--- routes/borrowHistory.routes.js
+++ routes/borrowHistory.routes.js
@@ -4,13 +4,13 @@ const requireRole = require("../middlewares/role.middleware");
 const auth = require("../middlewares/auth.middleware");
 
 // ดูประวัติทั้งหมด
-router.get("/", auth, requireRole(["admin", "staff"]), historyCtrl.list);
+router.get("/", auth, requireRole(["admin", "users"]), historyCtrl.list);
 
 // ดูประวัติของอุปกรณ์ตัวเดียว
 router.get(
   "/equipment/:equipmentId",
   auth,
-  requireRole(["admin", "staff"]),
+  requireRole(["admin", "users"]),
   historyCtrl.listByEquipment,
 );
 
